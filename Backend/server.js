71990e8c047023177b70bb21eed8d5cfdac82f1a
manan75 <4696.stkabirdin@gmail.com>
2025-03-28 15:51:02 +0530

    Started actual backend

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require("cors")
+const db = require("./db")
+
+//connecting with express and cors
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/message", (req,res)=>{
+    console.log(req.body.message)
+    res.json({reply: "got the message"})
+});
+
+app.get("/table", (req,res)=>{
+    const q = "SELECT * FROM NewSong"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.get("/songs", (req,res)=>{
+    const q = "SELECT * FROM NewSong"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        res.json(data)
+        
+    })
+})
+app.get("/manan", (req,res)=>{
+    res.json("manan is here")
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
