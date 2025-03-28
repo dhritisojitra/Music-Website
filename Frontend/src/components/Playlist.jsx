ccf3df8562bbb3de0ec7154fda5cd9e9b82d28db
manan75 <4696.stkabirdin@gmail.com>
2025-03-28 18:18:12 +0530

    fixed tailwind stuff

--- Frontend/src/components/Playlist.jsx
+++ Frontend/src/components/Playlist.jsx
@@ -1,33 +1,46 @@
 import Ok from '../Ok'
 import { Play } from "lucide-react";
 
-function Playlist(){
-    return(
-        <>
-        <Ok/>
-        <div className="bg-gradient-to-b from purple-950 to-black">
-        <div className="flex items-center justify-between bg-violet-600 h-30 mt-2 rounded-lg m-10 p-4">
-      {/* Playlist Name */}
-      <h2 className="text-white text-lg font-semibold">Playlist Name</h2>
+function Playlist() {
+  return (
+    <>
+      <Ok />
+      <div className="bg-gradient-to-b from-[#1a5dc9] to-black p-3 min-h-screen ">
+        <div className="  sm:flex flex flex-row text-white text-lg font-semibold bg-violet-900 h-20  rounded-lg p-4  mx-10 sm:mx-10">
+          <div className='flex-1'>Title</div>
+          <div className='flex-1'>Date Added</div>
+          <div className='flex-1'>No. of Songs</div>
+        </div>
+        <div className="flex sm:flex-row items-center justify-between bg-violet-600 h-30 mt-1 rounded-lg mx-10 p-4 cursor-pointer transition-transform duration-300 hover:scale-103 hover:bg-violet-400">
 
-      {/* Play Button */}
-      <button className="flex items-center justify-center w-12 h-12 bg-green-700 hover:bg-green-800 text-white rounded-full shadow-lg">
-        <Play className="w-6 h-6" />
-      </button>
-    </div>
+          {/* Playlist Name */}
+          <h2 className="flex-1 text-white text-lg font-semibold sm:text-left">Playlist Name</h2>
+          <h5 className='flex-1 text-white text-[36px]'>20/03/2025</h5>
+          <h5 className='flex-1 text-white text-2xl'>45</h5>
+          {/* Play Button */}
+        </div>
+        <div className="flex sm:flex-row items-center justify-between bg-violet-600 h-30 mt-1 rounded-lg mx-10 p-4 cursor-pointer transition-transform duration-300 hover:scale-103 hover:bg-violet-400">
 
-    <div className="flex items-center justify-between bg-violet-600 h-30 mt-2 rounded-lg m-10 p-4">
-      {/* Playlist Name */}
-      <h2 className="text-white text-lg font-semibold">Playlist Name</h2>
+          {/* Playlist Name */}
+          <h2 className="flex-1 text-white text-lg font-semibold sm:text-left">Playlist Name</h2>
+          <h5 className='flex-1 text-white text-[36px]'>20/03/2025</h5>
+          <h5 className='flex-1 text-white text-2xl'>45</h5>
+          {/* Play Button */}
+        </div>
 
-      {/* Play Button */}
-      <button className="flex items-center justify-center w-12 h-12 bg-green-700 hover:bg-green-800 text-white rounded-full shadow-lg">
-        <Play className="w-6 h-6" />
-      </button>
-    </div>
-    </div>
-        </>
-    )
+        <div className="flex sm:flex-row items-center justify-between bg-violet-600 h-30 mt-1 rounded-lg mx-10 p-4 cursor-pointer transition-transform duration-300 hover:scale-103 hover:bg-violet-400">
+
+          {/* Playlist Name */}
+          <h2 className="flex-1 text-white text-lg font-semibold sm:text-left">Playlist Name</h2>
+          <h5 className='flex-1 text-white text-[36px]'>20/03/2025</h5>
+          <h5 className='flex-1 text-white text-2xl'>45</h5>
+          {/* Play Button */}
+        </div>
+
+
+      </div>
+    </>
+  )
 }
 
 export default Playlist;
