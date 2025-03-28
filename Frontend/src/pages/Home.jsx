9d404daace4ad8ef2852dccd44d3385e199f6bf5
manan75 <4696.stkabirdin@gmail.com>
2025-03-28 15:43:37 +0530

    changed directories

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Ok from '../Ok'
+import ArtistCard from '../components/ArtistCard'
+
+function Home(){
+  return(
+    <>
+      <Ok/>
+      <ArtistCard/>
+    </>
+  )
+}
+
+export default Home;
